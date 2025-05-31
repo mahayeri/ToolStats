1710ac326ad583b93674dfae49c60d0a6dba02aa
Ali <m.a.haery110@gmail.com>
2025-05-31 20:14:42 +0100

    refactor(structure): move WeatherForecast to proper model location

---
+++ ToolStats.Web/WeatherForecast.cs
@@ -0,0 +1,9 @@
+﻿namespace ToolStats.Web;
+
+public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    /// <summary>
+    /// Gets the temperature in degrees Fahrenheit, calculated from <see cref="TemperatureC"/>.
+    /// </summary>
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
